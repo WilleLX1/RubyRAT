c40a9cdfec3ff6e9c4704b864af88cc96f70f8c6
Hacker-Service <68472201+WilleLX1@users.noreply.github.com>
2023-08-27 17:08:57 +0200

    Start of builder

---
+++ RubyRAT-Builder/RubyRAT-Builder/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RubyRAT_Builder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Void for generating the Python payload with the right Token
+        public void GeneratePayload()
+        {
+
+        }
+
+        private void btnGeneratePy_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
